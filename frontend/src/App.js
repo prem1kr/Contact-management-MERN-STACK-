2fa83a53723c8325bd8506c204f90edfa041652b
prem kumar <151729765+prem1kr@users.noreply.github.com>
2026-01-08 21:11:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/contacts")
+    fetch("https://contact-management-backend-jzpk.onrender.com/api/contacts")
       .then(res => res.json())
       .then(setContacts);
   }, []);
@@ -16,7 +16,7 @@ export default function App() {
   };
 
   const deleteContact = async (id) => {
-    await fetch(`http://localhost:5000/api/contacts/${id}`, {
+    await fetch(`https://contact-management-backend-jzpk.onrender.com/api/contacts/${id}`, {
       method: "DELETE"
     });
     setContacts(contacts.filter(c => c._id !== id));
